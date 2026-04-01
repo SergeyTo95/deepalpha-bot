aa1be772c54b9fd0d5f5a01f79a942fe4c352391
Serget_D <32731252+SergeyTo95@users.noreply.github.com>
2026-04-01 22:31:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-import os
-import aiohttp
-from aiogram.utils import executor
-import telegram_bot  # <-- добавь эту строку
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
+import telegram_bot
+from aiogram.utils import executor
+
 if __name__ == "__main__":
     executor.start_polling(telegram_bot.dp, skip_updates=True)
-  
