c2e517cdde0e8c479fb17cb59a84d30256d6cd31
Serget_D <32731252+SergeyTo95@users.noreply.github.com>
2026-04-02 19:19:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 import telegram_bot
 from aiogram.utils import executor
-
 from bot.admin import register_admin
 
-register_admin(dp)
+register_admin(telegram_bot.dp)
 
 if __name__ == "__main__":
     executor.start_polling(telegram_bot.dp, skip_updates=True)
